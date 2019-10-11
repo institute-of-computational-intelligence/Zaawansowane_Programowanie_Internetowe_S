cdd873f50fab08fe929146864a64edde2f6d5354
plubaszewski <piotrlubaszewski@gamil.com>
2019-10-11 09:30:08 +0200

    1

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,36 @@
 var htmlWebpackPlugin = require("html-webpack-plugin");
 var path = require("path");
+
+module.exports = {
+    mode: "development",
+    devtool: "sourcemap",
+    entry: {
+        app: "./src/index.ts"
+    },
+    output: {
+        path: path.join(__dirname, "./build/"),
+        filename: "bundle.js",
+    },
+    watch: true,
+    resolve: { extensions: [".js", ".ts"]},
+    devServer: {
+        contentBase: path.join(__dirname, "./build/"),
+        port: 9000
+    },
+    module: {
+        rules: [
+            { test:/\.ts$/, loader: "ts-loader"}
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            hash: true,
+            title: "Nasza pierwsza aplikacja Typescript",
+            myPageHeader: 'Nasza pierwsza aplikacja Typescript',
+            template: './build/index.html',
+            chunks: ['vendor', 'shared', 'app'],
+            path:  path.join(__dirname, "./build/"),
+            filename: 'index.html'
+        })
+    ]
+}
