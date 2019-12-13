454eb5ea0d6829b06b05c8e6b964aaabae691274
Rafal G <ra@ww.pl>
2019-12-13 18:22:11 +0100

    WebApiTemplate

---
+++ WebApiTemplate/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiTemplate.DAL;
+
+namespace WebApiTemplate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+        public ValuesController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+           return Ok($"Get {id}");
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] string model)
+        {
+            return Ok("POST");
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string model)
+        {
+             return Ok("PUT");
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Delete {id}");
+        }
+    }
+}
